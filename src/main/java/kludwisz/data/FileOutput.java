a3c8073bae2fa0ef2f4dff8a2d8d0c52a0b20258
Kludwisz <kludwisz@gmail.com>
2025-03-19 20:30:28 +0100

    add formatter and file output

---
+++ src/main/java/kludwisz/data/FileOutput.java
@@ -0,0 +1,17 @@
+package kludwisz.data;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileOutput {
+    public static synchronized void appendToFile(String filename, String text) {
+        try {
+            FileWriter fileWriter = new FileWriter(filename, true);
+            fileWriter.append(text).append("\n");
+            fileWriter.close();
+        }
+        catch (IOException e) {
+            System.err.println("Couldn't append to file: " + text);
+        }
+    }
+}
---
+++ src/main/java/kludwisz/data/Formatter.java
@@ -0,0 +1,19 @@
+package kludwisz.data;
+
+import com.seedfinding.mccore.state.Dimension;
+import com.seedfinding.mccore.util.math.Vec3i;
+import com.seedfinding.mccore.util.pos.CPos;
+
+public class Formatter {
+    public static String tpCommand(CPos chunkPos, int y, Dimension dimension) {
+        return String.format("/execute in minecraft:%s run tp @p %d %d %d", dimension.getName(), chunkPos.getX() * 16, y, chunkPos.getZ() * 16);
+    }
+
+    public static String tpCommand(Vec3i pos, Dimension dimension) {
+        return String.format("/execute in minecraft:%s run tp @p %d %d %d", dimension.getName(), pos.getX(), pos.getY(), pos.getZ());
+    }
+
+    public static String tpCommand(Vec3i pos) {
+        return String.format("/tp @p %d %d %d", pos.getX(), pos.getY(), pos.getZ());
+    }
+}
