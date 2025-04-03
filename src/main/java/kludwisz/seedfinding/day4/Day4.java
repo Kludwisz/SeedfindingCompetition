f7eb441fd519bd9c9fde04cecc391f65c1e2607f
Kludwisz <kludwisz@gmail.com>
2025-04-03 20:30:09 +0200

    start working on 2nd iter

--- src/main/java/kludwisz/seedfinding/day4/Day4.java
+++ src/main/java/kludwisz/seedfinding/day4/Day4.java
@@ -8,17 +8,22 @@ import java.util.stream.LongStream;
 public class Day4 {
     public static void main(String[] args) {
         // let's first find a seed with one cluster of half beds
-        List<HalfBedFinder.Result> results = HalfBedFinder.run(0, 10_000_000L);
+        List<HalfBedFinder.Result> results = HalfBedFinder.run(100_000_000L, 200_000_000L);
 
         // map the results to lists of 5k worldseeds each (checkable via cubiomes)
         results.forEach(r -> {
             SeedList list = new SeedList();
 
-            LongStream.range(0L, 5_000L)
+            LongStream.range(0L, 100L)
                     .map(upper -> (upper << 48) | r.structureSeed())
                     .forEach(worldSeed -> list.addEntry(List.of(worldSeed)));
 
-            list.appendToFile("src/main/resources/day4_A.txt");
+            list.appendToFile("src/main/resources/day4_D.txt");
         });
     }
+
+    // debug
+//    public static void main(String[] args) {
+//        HalfBedFinder.debug(5066549583033058L);
+//    }
 }
--- src/main/java/kludwisz/seedfinding/day4/HalfBedFinder.java
+++ src/main/java/kludwisz/seedfinding/day4/HalfBedFinder.java
@@ -32,13 +32,21 @@ public class HalfBedFinder {
         return results;
     }
 
+    // iteration 1 of the finder
+
     public static void checkStructureSeed(long seed, ArrayList<Result> results) {
         // get positions of tc and ac within the 0,0 region
         CPos tc = chambers.getInRegion(seed, 0, 0, rand);
         CPos ac = city.getInRegion(seed, 0, 0, rand);
-        if (tc.distanceTo(ac, DistanceMetric.EUCLIDEAN) < 6 || tc.distanceTo(ac, DistanceMetric.EUCLIDEAN) > 10)
+//        if (tc.distanceTo(ac, DistanceMetric.EUCLIDEAN) < 4 || tc.distanceTo(ac, DistanceMetric.EUCLIDEAN) > 9)
+//            return;
+//        // not close but not that far
+
+        // more specific params
+        int dx = tc.getX() - ac.getX(); // delta goes from ac to tc
+        int dz = tc.getZ() - ac.getZ();
+        if ((dx != 6 && dz != -1) && (dx != -5 && dz != -3))
             return;
-        // not close but not that far TODO tuning
 
         // get position of bed cluster for trial chambers
         rand.setCarverSeed(seed, tc.getX(), tc.getZ(), VERSION);
@@ -48,11 +56,14 @@ public class HalfBedFinder {
         BlockRotation startPieceRotation = rand.getRandom(BlockRotation.values());
         BPos bedVec = BedVectors.getVectorFor(startPieceRotation);
         BPos bedCluster = tc.toBlockPos(pickedY).add(bedVec);
+        if (bedCluster.distanceTo(BPos.ORIGIN, DistanceMetric.CHEBYSHEV) > 190)
+            return;
 
         // check if any of the ancient city towers generate at the correct place
         acgen.generate(seed, ac.getX(), ac.getZ(), rand);
 
-        for (var piece : acgen.pieces) {
+        for (int i = 0; i < acgen.piecesLen; i++) {
+            var piece = acgen.pieces[i];
             final String name = piece.getName();
             if (name.equals("structures/tall_ruin_1") || name.equals("structures/tall_ruin_3")) {
                 Vec3i center = piece.box.getCenter();
@@ -76,6 +87,61 @@ public class HalfBedFinder {
     private static boolean checkTrialChamberLayout(long seed, CPos tc) {
         tcgen.generate(seed, tc.getX(), tc.getZ(), rand);
         return tcgen.getPieces().stream().anyMatch(piece -> piece.getName().equals("intersection/intersection_2"));
+    }
+
+    // iteration 2 of the finder
+
+    public static void checkStructureSeed2(long seed, ArrayList<Result> results) {
+        // filter positions of tc and ac within the 0,0 and -1,0 regions
+        CPos tc_00 = chambers.getInRegion(seed, 0, 0, rand);
+        CPos ac_00 = city.getInRegion(seed, 0, 0, rand);
+
+        // more specific params
+        int dx = tc.getX() - ac.getX(); // delta goes from ac to tc
+        int dz = tc.getZ() - ac.getZ();
+        if ((dx != 6 && dz != -1) && (dx != -5 && dz != -3))
+            return;
+
+        // get position of bed cluster for trial chambers
+        rand.setCarverSeed(seed, tc.getX(), tc.getZ(), VERSION);
+        int pickedY = rand.nextInt(21) - 41;
+        if (pickedY + 8 != -33) // effectively, we need nextInt(21) == 0
+            return;
+        BlockRotation startPieceRotation = rand.getRandom(BlockRotation.values());
+        BPos bedVec = BedVectors.getVectorFor(startPieceRotation);
+        BPos bedCluster = tc.toBlockPos(pickedY).add(bedVec);
+        if (bedCluster.distanceTo(BPos.ORIGIN, DistanceMetric.CHEBYSHEV) > 190)
+            return;
+
+        // check if any of the ancient city towers generate at the correct place
+        acgen.generate(seed, ac.getX(), ac.getZ(), rand);
+
+        for (int i = 0; i < acgen.piecesLen; i++) {
+            var piece = acgen.pieces[i];
+            final String name = piece.getName();
+            if (name.equals("structures/tall_ruin_1") || name.equals("structures/tall_ruin_3")) {
+                Vec3i center = piece.box.getCenter();
+                if (center.getX() == bedCluster.getX() && center.getZ() == bedCluster.getZ()) {
+                    if (!checkTrialChamberLayout(seed, tc))
+                        return;
+
+                    System.out.println("Found seed with half beds: " + seed);
+                    System.out.println("Bed cluster: " + bedCluster);
+                    System.out.println("Ancient city tower: " + center);
+                    System.out.println("Rotation: " + startPieceRotation.name());
+                    System.out.println("TC: " + tc.toBlockPos(pickedY));
+                    System.out.println("AC: " + ac.toBlockPos(0) + "\n");
+                    results.add(new Result(seed, tc, ac));
+                    return;
+                }
+            }
+        }
+    }
+
+    public static void debug(long seed) {
+        CPos pos = city.getInRegion(seed, 0, 0, rand);
+        acgen.generate(seed, pos.getX(), pos.getZ(), rand);
+
     }
 
     public record Result(long structureSeed, CPos trialChamber, CPos ancientCity) {}
