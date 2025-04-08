620130beba519cb50188ac3e44c71dec94786b2a
Kludwisz <kludwisz@gmail.com>
2025-04-08 19:59:51 +0200

    document half-bed code

--- src/main/java/kludwisz/seedfinding/day4/HalfBedFinder.java
+++ src/main/java/kludwisz/seedfinding/day4/HalfBedFinder.java
@@ -1,14 +1,12 @@
 package kludwisz.seedfinding.day4;
 
 import com.seedfinding.mccore.rand.ChunkRand;
-import com.seedfinding.mccore.state.Dimension;
 import com.seedfinding.mccore.util.block.BlockRotation;
 import com.seedfinding.mccore.util.math.DistanceMetric;
 import com.seedfinding.mccore.util.math.Vec3i;
 import com.seedfinding.mccore.util.pos.BPos;
 import com.seedfinding.mccore.util.pos.CPos;
 import com.seedfinding.mcfeature.structure.Igloo;
-import com.seedfinding.mcfeature.structure.RuinedPortal;
 import kludwisz.ancientcity.AncientCity;
 import kludwisz.ancientcity.AncientCityGenerator;
 import kludwisz.generator.TrialChambersGenerator;
@@ -27,8 +25,14 @@ public class HalfBedFinder {
     private static final TrialChambers chambers = new TrialChambers(VERSION);
     private static final NewVillage village = new NewVillage(VERSION);
     private static final Igloo igloo = new Igloo(VERSION);
-    private static final RuinedPortal portal = new RuinedPortal(Dimension.OVERWORLD, VERSION);
     private static final ChunkRand rand = new ChunkRand();
+
+    /**
+     * All the code below is the first iteration of the quad-bed finder. It simply searches
+     * for the correct configuration of a Trial Chambers and an Ancient City. This was used
+     * to get an initial estimate of how rare such features were, and to find common chunk
+     * offsets at which the structures can generate suitable layouts.
+     */
 
     public static List<Result> run(long start, long end) {
         ArrayList<Result> results = new ArrayList<>();
@@ -38,16 +42,10 @@ public class HalfBedFinder {
         return results;
     }
 
-    public static List<Result> run2(long start, long end) {
-        ArrayList<Result> results = new ArrayList<>();
-        for (long seed = start; seed < end; seed++) {
-            checkStructureSeed2(seed, results);
-        }
-        return results;
-    }
-
-    // iteration 1 of the finder
-
+    /**
+     * Checks if the given structure seed generates a Trial Chambers and an Ancient City that
+     * have such layouts that a quad-bed cluster is guaranteed to generate (excluding biome checks).
+     */
     public static void checkStructureSeed(long seed, ArrayList<Result> results) {
         // get positions of tc and ac within the 0,0 region
         CPos tc = chambers.getInRegion(seed, 0, 0, rand);
@@ -98,13 +96,36 @@ public class HalfBedFinder {
         }
     }
 
+    /**
+     * Checks if the trial chamber in the given position generates the correct intersection piece - intersection_2
+     */
     private static boolean checkTrialChamberLayout(long seed, CPos tc) {
         tcgen.generate(seed, tc.getX(), tc.getZ(), rand);
         return tcgen.getPieces().stream().anyMatch(piece -> piece.getName().equals("intersection/intersection_2"));
     }
 
-    // iteration 2 of the finder
+    /**
+     * The following code is the second iteration of the quad-bed finder. Its search parameters
+     * are fine-tuned using the data gathered in the first iteration. While initially the plan
+     * was to find a second Ancient City - Trial Chambers pair to get more half-beds, it turned
+     * out to be too slow and rare to find within the competition's limited time. Ultimately,
+     * a simple approach of checking for an igloo near a village sufficed to find seeds with
+     * 5 half-beds. With some major adjustments, this kind of search could potentially yield
+     * between 6 and 8 half-beds.
+     */
 
+    public static List<Result> run2(long start, long end) {
+        ArrayList<Result> results = new ArrayList<>();
+        for (long seed = start; seed < end; seed++) {
+            checkStructureSeed2(seed, results);
+        }
+        return results;
+    }
+
+    /**
+     * Checks the relative positions of the Trial Chambers and Ancient City using hardcoded
+     * offset vectors that were found in the first iteration of the quad-bed finder.
+     */
     private static boolean filterStructurePos(CPos tc, CPos ac) {
         if (tc.getX() > 15) return false;
         int dx = tc.getX() - ac.getX(); // delta goes from ac to tc
@@ -112,6 +133,10 @@ public class HalfBedFinder {
         return (dx == 6 && dz == -1) || (dx == -5 && dz == -3);
     }
 
+    /**
+     * Returns the exact block position of the center of a 4-bed cluster assuming that
+     * the Trial Chambers generates with the intersection_2 piece.
+     */
     private static BPos getBedCluster(long seed, CPos tc) {
         // get position of bed cluster for trial chambers
         rand.setCarverSeed(seed, tc.getX(), tc.getZ(), VERSION);
@@ -123,6 +148,10 @@ public class HalfBedFinder {
         return tc.toBlockPos(pickedY).add(bedVec);
     }
 
+    /**
+     * Very similar to the first iteration, expect this time we're also looking for
+     * a close village and igloo.
+     */
     public static void checkStructureSeed2(long seed, ArrayList<Result> results) {
         // filter positions of tc and ac within the 0,0 region for the hard cluster
         CPos tc_00 = chambers.getInRegion(seed, 0, 0, rand);
@@ -157,6 +186,9 @@ public class HalfBedFinder {
         for (int i = 0; i < acgen.piecesLen; i++) {
             var piece = acgen.pieces[i];
             final String name = piece.getName();
+
+            // these two structures always generate a hashtag-like roof pattern which, when intersecting with
+            // the Trial Chambers bed cluster, alwyas produces 4 half-beds.
             if (name.equals("structures/tall_ruin_1") || name.equals("structures/tall_ruin_3")) {
                 Vec3i center = piece.box.getCenter();
                 if (center.getX() == cluster1.getX() && center.getZ() == cluster1.getZ()) {
